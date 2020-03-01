d1f2ac7702f28ce6d659ce579d9dbca640c724e4
TheoBtu <55090599+TheoBtu@users.noreply.github.com>
2020-03-01 21:05:46 +0100

    projet final

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,62 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-content>
+      <div class="nav-bar">
+        <h1> Nos amis les animaux </h1>
+        <v-icon> fas fa-paw </v-icon>
+      </div>
+      <v-carousel hide-delimiters height="656px" cycle>
+        <v-carousel-item
+                v-for="(item,i) in items"
+                :key="i"
+                :src="item.src"
+        ></v-carousel-item>
+      </v-carousel>
+      <div class="grid">
+      <animaux class="mt-5 pb-5 " v-for="animal in animaux" :name ="animal.name" :taille = "animal.taille" :image="animal.image" :poids="animal.poids" :description="animal.description" :type="animal.type" :key="animal.id">
+      </animaux>
+      </div>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import animaux from './components/HelloWorld';
 
 export default {
   name: 'App',
+
   components: {
-    HelloWorld
+    animaux
+  },
+
+  data:function(){
+  return{
+    animaux:[
+      {name: "Le Chien", type:"Mammifères", taille: 0.45, poids: 15,image:"https://resize.prod.docfr.doc-media.fr/img/var/doctissimo/storage/images/fr/www/animaux/chien/soins-chien/bain-pour-un-chien/723393-2-fre-FR/bain-pour-un-chien.jpg", description:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores ex explicabo ipsa ipsum laudantium libero numquam quos ut voluptatum? A accusantium consequatur hic incidunt quasi quia, sed sit veniam voluptatem?"},
+      {name: "Le Chat", type:"Félin", taille: 0.25, poids: 2,image:"https://www.wikichat.fr/wp-content/uploads/sites/2/comment-soigner-une-plaie-dun-chat.jpg", description:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores ex explicabo ipsa ipsum laudantium libero numquam quos ut voluptatum? A accusantium consequatur hic incidunt quasi quia, sed sit veniam voluptatem?"},
+      {name: "Le Cheval", type:"Mammifères", taille: 2.20, poids: 450,image:"https://www.cotelandesnaturetourisme.com/app/uploads/cotelandesnature/2019/02/saintjulien-4804-clntsophiepawlak-1920x1278.jpg", description:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores ex explicabo ipsa ipsum laudantium libero numquam quos ut voluptatum? A accusantium consequatur hic incidunt quasi quia, sed sit veniam voluptatem?"},
+      {name: "Le Hamster", type:"Rongeur", taille: 0.07, poids: 0.300,image:"https://www.zooplus.fr/magazine/wp-content/uploads/2019/08/race-de-hamster-1.jpg", description:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores ex explicabo ipsa ipsum laudantium libero numquam quos ut voluptatum? A accusantium consequatur hic incidunt quasi quia, sed sit veniam voluptatem?"},
+      {name: "Le Poisson", type:"Poisson", taille: 0.03 , poids: 1,image:"https://www.fishipedia.fr/wp-content/uploads/2016/01/poisson_rouge-2.jpg", description:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores ex explicabo ipsa ipsum laudantium libero numquam quos ut voluptatum? A accusantium consequatur hic incidunt quasi quia, sed sit veniam voluptatem?"},
+    ],
+    items: [
+      {
+        src: 'https://resize.prod.docfr.doc-media.fr/img/var/doctissimo/storage/images/fr/www/animaux/chien/soins-chien/bain-pour-un-chien/723393-2-fre-FR/bain-pour-un-chien.jpg',
+      },
+      {
+        src: 'https://www.wikichat.fr/wp-content/uploads/sites/2/comment-soigner-une-plaie-dun-chat.jpg',
+      },
+      {
+        src: 'https://www.cotelandesnaturetourisme.com/app/uploads/cotelandesnature/2019/02/saintjulien-4804-clntsophiepawlak-1920x1278.jpg',
+      },
+      {
+        src: 'https://www.zooplus.fr/magazine/wp-content/uploads/2019/08/race-de-hamster-1.jpg',
+      },
+      {
+        src: 'https://www.fishipedia.fr/wp-content/uploads/2016/01/poisson_rouge-2.jpg',
+      },
+    ],
   }
 }
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,150 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <v-hover
+          v-slot:default="{ hover }"
+          close-delay="200"
+  >
+      <v-card
+              :elevation="hover ? 16 : 2"
+              class="mx-auto"
+              max-width="400"
+      >
+        <v-img
+                class="white--text align-end"
+                height="200px"
+                :src="image"
+        >
+          <v-card-title class="title">{{name}}</v-card-title>
+        </v-img>
+
+        <v-card-text class="text--primary">
+          <div>{{description}}</div>
+          <br>
+          <div>La taille moyenne est de {{taille}} m </div>
+          <div>Le poids moyen est de {{poids}} kg</div>
+          <v-divider></v-divider>
+          <v-card-text> Article posté durant l'année {{ today | toDate }} </v-card-text>
+        </v-card-text>
+        <div class="text-center">
+          <v-dialog
+                  v-model="dialog"
+                  width="500"
+          >
+            <template v-slot:activator="{ on }">
+              <v-btn
+                      color="primary"
+                      dark
+                      v-on="on"
+              >
+                Voir plus
+              </v-btn>
+            </template>
+
+            <v-card>
+              <v-card-title
+                      class="headline grey lighten-2"
+                      primary-title
+              >
+                {{name}}
+              </v-card-title>
+
+              <v-card-text>
+                <div>{{description}}</div>
+                <br>
+                <div>La taille moyenne est de {{taille}} m </div>
+                <div>Le poids moyen est de {{poids}} kg</div>
+                  <br>
+                  <v-btn @click="showtype=true"> Afficher le type </v-btn>
+                  <br>
+                  <div>
+                      <p v-if="showtype=true"> {{name}} est de type {{type}}
+                      </p>
+                      <p v-else> Pas de type affiché </p>
+                  </div>
+              </v-card-text>
+              <v-divider></v-divider>
+            </v-card>
+          </v-dialog>
+        </div>
+      </v-card>
+  </v-hover>
 </template>
 
-<script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
-</script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+<style>
+  .content{
+    height: 100%;
+    width: 100%;
+  }
+  .title{
+    color: #252525;
+  }
+  .text--primary{
+    color: #252525;
+  }
+  h1{
+    font-size: 30px;
+    font-family: Roboto;
+    color: #e3e3e3;
+  }
+  .nav-bar{
+    height: 65px;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: #252525
+  }
+  .grid{
+      display: grid;
+      grid-template-columns: repeat(3, 1fr);
+      grid-template-rows: repeat(2, 1fr);
+      grid-column-gap: 0px;
+      grid-row-gap: 0px;
+  }
+  .boutontype{
+    padding-top: 200px;
+  }
 </style>
+
+<script>
+  export default {
+    name: 'animaux',
+    props: {
+      name:String,
+      taille:Number,
+      poids:Number,
+      description:String,
+      image:String,
+      type:String,
+    },
+    filters : {
+      toDate : function (dateValue) {
+        return new Date(dateValue).getFullYear();
+      }
+    },
+    data () {
+      return {
+          showtype:false,
+        today : Date.now(),
+        items: [
+          {
+            src: 'https://resize.prod.docfr.doc-media.fr/img/var/doctissimo/storage/images/fr/www/animaux/chien/soins-chien/bain-pour-un-chien/723393-2-fre-FR/bain-pour-un-chien.jpg',
+          },
+          {
+            src: 'https://www.wikichat.fr/wp-content/uploads/sites/2/comment-soigner-une-plaie-dun-chat.jpg',
+          },
+          {
+            src: 'https://www.cotelandesnaturetourisme.com/app/uploads/cotelandesnature/2019/02/saintjulien-4804-clntsophiepawlak-1920x1278.jpg',
+          },
+          {
+            src: 'https://www.zooplus.fr/magazine/wp-content/uploads/2019/08/race-de-hamster-1.jpg',
+          },
+          {
+            src: 'https://www.fishipedia.fr/wp-content/uploads/2016/01/poisson_rouge-2.jpg',
+          },
+        ],
+         // ICI POUR LA PROCHAINE DATA //
+      }
+    },
+  }
+</script>
